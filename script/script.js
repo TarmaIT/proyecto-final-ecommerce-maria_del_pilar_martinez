73db4a758d8cc2c9941dac3a0f6941b218f7b392
TarmaIT <pilar.it@isosoftech.com.ar>
2025-12-11 15:55:02 -0300

    ver0.2

---
+++ script/script.js
@@ -0,0 +1,62 @@
+
+
+const servicios = [
+    {
+    id:1,
+    nombre:"Mantenimientos mensuales",
+    descripcion:"Mantenimientos mensuales de pc",
+    precio:50.00
+    },
+    {
+    id:2,
+    nombre:"Redes y conectividad",
+    descripcion:"Redes de pc",
+    precio:60.00
+    },
+    {
+    id:3,
+    nombre:"Seguridad informatica",
+    descripcion:"Mantenimientoy pentesting",
+    precio:100.00
+    },
+    {
+    id:4,
+    nombre:"Soporte tecnico",
+    descripcion:"Hora de soporte remoto",
+    precio:30.00
+    }
+]
+
+
+
+function mostarServicios(catalogo, titulo){
+    
+    console.log(`\n ----- ${titulo} ----`);
+    for (const {id, nombre, precio} of catalogo){
+        console.log(`ID: ${id} | servicio: ${nombre} | precio: ${precio.toFixed(2)} USD`);
+
+    }
+}
+
+function crearServicio(idser, nombreser, descripcionser, precioser){
+ const servicioNuevo=[
+    {
+     id: idser,
+     nombre: nombreser,
+     descripcion: descripcionser,
+     precio: precioser
+    }
+
+ ]
+ return servicioNuevo;
+
+}
+ 
+
+
+mostarServicios(crearServicio (5, "Retiro y entrega de equipo", "Retirar y entragar equipo a domicilio", 30.00), "Servicio nuevo");
+     
+
+
+
+
